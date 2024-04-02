06145299563493c3cc52ac040ac93f6704833c55
Rohan K <khopkar.rohan@gmail.com>
2024-04-02 18:47:41 -0400

    'llm'

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,22 @@ import streamlit as st
 import snowflake.connector
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
+import pandas as pd
+import torch
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_URL = os.getenv('')
+headers = {
+	"Accept" : "application/json",
+	"Authorization": os.getenv('HF_API_TOKEN'),
+	"Content-Type": "application/json" 
+}
 
 # Function to connect to Snowflake database
 def connect_to_snowflake(user, password, account, database, warehouse, schema):
@@ -30,9 +46,64 @@ def connect_to_databricks(jdbc_url, user, password, driver):
     sql_context = SQLContext(spark)
     return spark, sql_context
 
+
+def get_embed(text):
+    tokenizer = AutoTokenizer.from_pretrained("deepset/roberta-base-squad2")
+    model = AutoModelForQuestionAnswering.from_pretrained("deepset/roberta-base-squad2")
+
+    # Tokenize input text
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+    
+    # Forward pass through the model
+    with torch.no_grad():
+        outputs = model(**inputs)
+    
+    # Get the embeddings from the model output
+    embeddings = outputs.last_hidden_state
+    
+    return embeddings
+
+def generate_corpus(cursor, filename='corpus.csv', corpus_length=100):
+    corpus_df = pd.read_csv(filename, sep='\n', names=['sentence', 'embedding'] ,header=False)
+    
+    cursor.execute("SHOW TABLES")
+    tables = [row[1] for row in cursor.fetchall()]
+    s = f"There are {len(tables)} tables in the schema.\n"
+    corpus_df.loc[len(corpus_df.index)] = [s, get_embed(s)]
+    return corpus_df
+
+
+#Helper function for embedding similarity
+def cosine_similarity(a, b):
+  dot_product = sum(x * y for x, y in zip(a, b))
+  magnitude_a = sum(x * x for x in a)**0.5
+  magnitude_b = sum(x * x for x in b)**0.5
+  return dot_product / (magnitude_a * magnitude_b)
+
+
+def semantic_search(user_input, df, context_length):
+    
+    df['cos_dists'] = df.embedding.apply(lambda x: 1 - cosine_similarity(
+    list(map(float,
+             x.strip('][').split(', '))), user_input))
+
+    best_inds = df.cos_dists.nlargest(context_length).index.values
+
+    #get text of most similar embedding/
+    most_sim_df = df.sentence.iloc[best_inds]
+    most_similar_text = '; '.join([i for i in most_sim_df.values])
+    context_string = most_similar_text[1:]
+    return context_string
+
+
+def query(payload):
+	response = requests.post(os.getenv('HF_API_URL'), headers=headers, json=payload)
+	return response.json()
+
+
 # Main function to display database information
 def main():
-    st.title("Database Information")
+    st.title("dRAG - A Database Informed Chatbot :dragon_face:")
 
     # Database selection
     db_option = st.radio("Select Database:", ("Snowflake", "Databricks"))
@@ -50,16 +121,29 @@ def main():
             conn = connect_to_snowflake(user, password, account, database, warehouse, schema)
             st.success("Connected to Snowflake")
 
-            # Fetch and display tables and columns
+            # Create document corpus and embeddings
             cursor = conn.cursor()
-            cursor.execute("SHOW TABLES")
-            tables = [row[1] for row in cursor.fetchall()]
-            for table in tables:
-                st.write(f"## Table: {table}")
-                cursor.execute(f"DESCRIBE TABLE {table}")
-                columns = cursor.fetchall()
-                for column in columns:
-                    st.write(f"- {column[0]} | {column[1]}")
+            corpus_df = generate_corpus(cursor)
+            
+            # Q&A
+            user_input = st.text_input("Ask a question about your data:")
+            
+            output = query({
+                "context": semantic_search(user_input, corpus_df, 1),
+                "question": f'{user_input}',
+                "parameters": {}
+            })
+            
+            st.write(f':dragon_face:: {output}')
+            
+            # cursor.execute("SHOW TABLES")
+            # tables = [row[1] for row in cursor.fetchall()]
+            # for table in tables:
+            #     st.write(f"## Table: {table}")
+            #     cursor.execute(f"DESCRIBE TABLE {table}")
+            #     columns = cursor.fetchall()
+            #     for column in columns:
+            #         st.write(f"- {column[0]} | {column[1]}")
 
     elif db_option == "Databricks":
         st.sidebar.subheader("Databricks Connection Configuration")
@@ -85,3 +169,5 @@ def main():
 # Run the main function
 if __name__ == "__main__":
     main()
+    
+
